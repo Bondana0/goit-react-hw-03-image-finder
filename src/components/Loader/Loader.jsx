d2a58a40f200f8d6cdbb75318050c385f91f1d96
Bondana0 <120127799+Bondana0@users.noreply.github.com>
2023-08-11 14:29:56 +0300

    fix

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -7,12 +7,12 @@ export const Loader = ({ isLoading }) => {
       {isLoading && (
         <ColorRing
           visible={true}
-          height="200"
-          width="200"
+          height="100"
+          width="100"
           ariaLabel="blocks-loading"
           wrapperStyle={{ margin: 'auto' }}
           wrapperClass="blocks-wrapper"
-          colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+          colors={['#f47e60', '#f47e60', '#f8b26a', '#f8b26a', '#f47e60']}
         />
       )}
     </>
